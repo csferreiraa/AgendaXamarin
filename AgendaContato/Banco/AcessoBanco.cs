855be30636a8929aeea28f58fd3d036578ea702f
Cleiton Ferreira <csferreiraa@gmail.com>
2019-12-10 15:44:53 -0300

    Add project files.

---
+++ AgendaContato/Banco/AcessoBanco.cs
@@ -0,0 +1,36 @@
+﻿using AgendaContato.Model;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendaContato.Banco
+{
+    public class AcessoBanco { 
+
+
+        private SQLiteConnection _conexao;
+        public AcessoBanco()
+        {
+            _conexao = new SQLiteConnection();
+        }
+
+        public List<Agenda> Consultar()
+        {
+            return null;
+        }
+        public void Cadastro(Agenda agenda)
+        {
+
+        }
+
+        public void Update(Agenda agenda)
+        {
+
+        }
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
