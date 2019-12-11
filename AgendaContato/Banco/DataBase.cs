5c11ce030a9f9555df13ce1f6887cbdcbcb81f2b
Cleiton Ferreira <csferreiraa@gmail.com>
2019-12-11 15:20:26 -0300

    Codificação DataBase

--- AgendaContato/Banco/DataBase.cs
+++ AgendaContato/Banco/DataBase.cs
@@ -2,14 +2,15 @@
 using SQLite;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xamarin.Forms;
 
 
 namespace AgendaContato.Banco
 {
-    public class DataBase { 
-
+    public class DataBase
+    {
 
         private SQLiteConnection _conexao;
         public DataBase()
@@ -18,25 +19,37 @@ namespace AgendaContato.Banco
             string url = dep.GetCaminho("database.sqlite");
 
             _conexao = new SQLiteConnection(url);
+            _conexao.CreateTable<Agenda>();
         }
 
         public List<Agenda> Consultar()
         {
-            return null;
+            return _conexao.Table<Agenda>().ToList();
+        }
+
+        public List<Agenda> Pesquisar(string palavra)
+        {
+            return _conexao.Table<Agenda>().Where(a => a.Nome.Contains(palavra)).ToList();
+        }
+
+        public Agenda GetId(int id)
+        {
+            return _conexao.Table<Agenda>().Where(a => a.Id == id).FirstOrDefault();
         }
 
         public void Create(Agenda agenda)
         {
-
+            _conexao.Insert(agenda);
         }
 
         public void Update(Agenda agenda)
         {
-
+            _conexao.Update(agenda);
         }
-        public void Delete(int id)
-        {
 
+        public void Delete(Agenda agenda)
+        {
+            _conexao.Delete(agenda);
         }
     }
 }
--- AgendaContato/Model/Agenda.cs
+++ AgendaContato/Model/Agenda.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using SQLite;
 namespace AgendaContato.Model
 {
+    [Table("Agenda")]
     public class Agenda
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
         public string Telefone { get; set; }
