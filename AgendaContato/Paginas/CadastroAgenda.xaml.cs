14e8a94c73baed175eee4132f57293007f856ad4
Cleiton Ferreira <csferreiraa@gmail.com>
2019-12-11 23:01:24 -0300

    Inserindo Contatos na Agenda

--- AgendaContato/Paginas/CadastroAgenda.xaml.cs
+++ AgendaContato/Paginas/CadastroAgenda.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AgendaContato.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +7,34 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using AgendaContato.Banco;
 
 namespace AgendaContato.Paginas
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CastradoAgenda : ContentPage
+    public partial class CadastroAgenda : ContentPage
     {
-        public CastradoAgenda()
+        public CadastroAgenda()
         {
             InitializeComponent();
+        }
+
+        public void SalvarAction(object sender, EventArgs args)
+        {
+            //TODO - Obter dados da tela
+            Agenda agenda = new Agenda();
+            agenda.Nome = Nome.Text;
+            agenda.Sobrenome = Sobrenome.Text;
+            agenda.Telefone = Telefone.Text;
+            agenda.TipoTelofone = (TipoTelefone.IsToggled) ? "Pessoal" : "Trabalho";
+            agenda.Email = Email.Text;
+            //TODO - Salvar informações no Banco
+
+            DataBase database = new DataBase();
+            database.Create(agenda);
+
+            //TODO - Voltar para a tela de pesquisa
+            Navigation.PopAsync();
         }
     }
 }
